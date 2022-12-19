d640ea535c17faa74e76d70ad950fd6371b9429d
0utKast <educaleft@gmail.com>
2022-12-19 07:16:45 +0000

    218.-CPPAvanzado
    Código VT 217 del Curso de CPP Avanzado, en el que vemos como añadir librerías externas como estáticas dentro de un proyecto C++

---
+++ CPPBasico218/SqliteStatic/SqliteStatic/SqliteStatic.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <sqlite3.h>
+
+int main()
+{
+    sqlite3* db;
+    char* zErrMsg = 0;
+    int rc;
+
+    rc = sqlite3_open("test.db", &db);
+
+    if (rc) {
+        std::cout << "Error opening SQLite3 database: " << sqlite3_errmsg(db) << std::endl;
+        sqlite3_close(db);
+        return 1;
+    }
+
+    std::cout << "Opened database successfully" << std::endl;
+
+    sqlite3_close(db);
+    return 0;
+}
